9ae9cf9a11109dd12a628ce9e22c24d9db139d16
frans <afriandifrans@gmail.com>
2023-02-05 11:17:52 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const API = axios.create({
     // baseURL: "http://localhost:5000/api/v1"
-    baseURL: "server-dumbsound-production.up.railway.app/api/v1"
+    baseURL: "https://server-dumbsound-production.up.railway.app/api/v1"
 })
 export const setAuthToken = (token) =>{
     if(token){
